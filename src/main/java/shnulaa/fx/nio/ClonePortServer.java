b1f4b0d8e786cb31dc958c50942c7569e1a91d1b
shnulaa <ashitawillbefine@gmail.com>
2016-01-21 18:40:37 +0800

    Signed-off-by: shnulaa <ashitawillbefine@gmail.com>

--- src/main/java/shnulaa/fx/nio/ClonePortServer.java
+++ src/main/java/shnulaa/fx/nio/ClonePortServer.java
@@ -1,8 +1,16 @@
 package shnulaa.fx.nio;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import shnulaa.fx.exception.NioException;
 
 /**
  * 
@@ -11,13 +19,43 @@ import java.nio.channels.Selector;
  */
 public class ClonePortServer extends NioServerBase {
 
+	private static Logger log = LoggerFactory.getLogger(NioServerBase.class);
+
+	private int port;
+
+	public ClonePortServer(int port) {
+		this.port = port;
+	}
+
 	@Override
 	protected Selector initSelector() {
-		return null;
+		try {
+			ServerSocketChannel server = SelectorProvider.provider().openServerSocketChannel();
+			server.bind(new InetSocketAddress(port));
+			Selector selector = Selector.open();
+			server.register(selector, SelectionKey.OP_ACCEPT);
+			return selector;
+		} catch (IOException e) {
+			String error = "IOException occurred when init selector..";
+			log.error(error, e);
+			throw new NioException(error);
+		}
 	}
 
 	@Override
 	protected boolean progressRequest(ChangeRequest changeRequest) throws IOException {
+		switch (changeRequest.type) {
+		case ChangeRequest.CHANGE_SOCKET_OP:
+			SelectionKey key = changeRequest.socket.keyFor(selector);
+			key.interestOps(changeRequest.op);
+			break;
+		case ChangeRequest.CLOSE_CHANNEL:
+
+			break;
+		default:
+			log.error("");
+			break;
+		}
 		return false;
 	}
 
